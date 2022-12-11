25502fb01b727efd630ce448955d61911747fec3
Davidlhevilla <92096052+Davidlhevilla@users.noreply.github.com>
2022-12-11 10:56:57 +0100

    Terminado y listo para enseñar

--- GymEjercicios/src/main/Main.java
+++ GymEjercicios/src/main/Main.java
@@ -19,36 +19,28 @@ public class Main {
 		
 		
 		
-
-		
-		
-		
-		
-//		Cliente cliente1=new Cliente("Miguel",38,"H",null);
-//		Cliente cliente2 =new Cliente("David",30,"H",null);
-//		Cliente cliente3=new Cliente("Jenaro",30,"H",null);
-//		Cliente cliente4= new Cliente("Pepe",25,"H",null);
 		ClienteDao cd=new ClienteDao();
-//		cd.insertar(cliente4);
-//		System.out.println(cd.buscarPorId(4));
-//		Cliente cliente = cd.buscarPorId(4);
 		
-//		cd.borrar(cliente4);
+		EjercicioDao ed=new EjercicioDao();
 		
-//		cd.insertar(cliente4);
-		
-//		Ejercicio abs = new Ejercicio("Abdominales",10,3,cliente);
-//		Ejercicio pressBanca = new Ejercicio("Press de banca",10,4,cliente4);
-//		
-//		EjercicioDao ed=new EjercicioDao();
-//		ed.insertar(abs);
-//		cd.insertar(cliente2);
-		
-//		ed.insertar(pressBanca);
-//		ed.insertar(abs);
+		//Creacion de cliente antes del ejercicio
+		Cliente c=new Cliente("Miguel",38,"H",null);
 		
 		
-//		EjercicioDao ejercicioDao = new EjercicioDao();
+		cd.insertar(c);
+		
+		Cliente miCliente=cd.buscarPorId(1);
+		
+		
+		//Creamos ejercicio a partir del buscado por id
+		Ejercicio e1=new Ejercicio("Press de banca",10,4,miCliente);
+		Ejercicio abs = new Ejercicio("Abdominales",10,3,miCliente);
+		
+		ed.insertar(e1);
+		ed.insertar(abs);
+		
+		
+
 		
 		ArrayList<Cliente> clientes = cd.buscarTodos();
 		
@@ -61,9 +53,7 @@ public class Main {
 			}
 			System.out.println("");
 		}
-//		Cliente cliente = cd.buscarPorId(1);
-//		cd.borrar(cliente);
-//		
+
 		
 		
 
